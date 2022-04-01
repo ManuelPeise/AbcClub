a5bc4ec596db514d27f603c0d3068e031451de50
Manuel Peise <manuel.p80@gmx.de>
2022-04-01 12:06:49 +0200

    add db migrator

---
+++ Abc-Club/Web.Api/Helpers/MigrationHelper.cs
@@ -0,0 +1,56 @@
+﻿using Data.AppData;
+using Data.UserContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Web.Api.Helpers
+{
+    public static class MigrationHelper
+    {
+        internal static void TryMigrateDatabases(IApplicationBuilder app)
+        {
+            var serviceProvider = app.ApplicationServices;
+            
+            Migrate<UserDataContext>(serviceProvider);
+
+            Migrate<AppDataContext>(serviceProvider); 
+        }
+
+        private static void Migrate<T>(IServiceProvider provider)
+        {
+
+            if (typeof(T) == typeof(UserDataContext))
+            {
+                using (var scope = provider.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<UserDataContext>();
+
+                    var created = context.Database.EnsureCreated();
+
+                    if (context.Database.GetPendingMigrations().Any())
+                    {
+                        context.Database.Migrate();
+                    }
+                }
+            }
+
+            if (typeof(T) == typeof(AppDataContext))
+            {
+                using (var scope = provider.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<AppDataContext>();
+
+                    var created = context.Database.EnsureCreated();
+
+                    if (context.Database.GetPendingMigrations().Any())
+                    {
+                        context.Database.Migrate();
+                    }
+                }
+            }
+        }
+    }
+}
